d1981d3b1822606140ce30871904fe609dd21fb4
Minhtandev <tannm20522002@gmail.com>
2023-04-10 20:04:27 +0700

    config vercel

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -4,47 +4,45 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="not-found">
-      <a href="https://codepen.io/uiswarup/full/yLzypyY" target="_blank">
-        <header className="top-header"></header>
+      <header className="top-header"></header>
 
-        {/* <!--dust particel--> */}
-        <div>
-          <div className="starsec"></div>
-          <div className="starthird"></div>
-          <div className="starfourth"></div>
-          <div className="starfifth"></div>
+      {/* <!--dust particel--> */}
+      <div>
+        <div className="starsec"></div>
+        <div className="starthird"></div>
+        <div className="starfourth"></div>
+        <div className="starfifth"></div>
+      </div>
+      {/* <!--Dust particle end---> */}
+
+      <div className="lamp__wrap">
+        <div className="lamp">
+          <div className="cable"></div>
+          <div className="cover"></div>
+          <div className="in-cover">
+            <div className="bulb"></div>
+          </div>
+          <div className="light"></div>
         </div>
-        {/* <!--Dust particle end---> */}
-
-        <div className="lamp__wrap">
-          <div className="lamp">
-            <div className="cable"></div>
-            <div className="cover"></div>
-            <div className="in-cover">
-              <div className="bulb"></div>
-            </div>
-            <div className="light"></div>
+      </div>
+      {/* <!-- END Lamp --> */}
+      <section className="error">
+        {/* <!-- Content --> */}
+        <div className="error__content">
+          <div className="error__message message">
+            <h1 className="message__title">Page Not Found</h1>
+            <p className="message__text">
+              We're sorry, the page you were looking for isn't found here. The
+              link you followed may either be broken or no longer exists. Please
+              try again, or take a look at our.
+            </p>
+          </div>
+          <div className="error__nav e-nav">
+            <Link to="/" className="e-nav__link"></Link>
           </div>
         </div>
-        {/* <!-- END Lamp --> */}
-        <section className="error">
-          {/* <!-- Content --> */}
-          <div className="error__content">
-            <div className="error__message message">
-              <h1 className="message__title">Page Not Found</h1>
-              <p className="message__text">
-                We're sorry, the page you were looking for isn't found here. The
-                link you followed may either be broken or no longer exists.
-                Please try again, or take a look at our.
-              </p>
-            </div>
-            <div className="error__nav e-nav">
-              <Link to="/" className="e-nav__link"></Link>
-            </div>
-          </div>
-          {/* <!-- END Content --> */}
-        </section>
-      </a>
+        {/* <!-- END Content --> */}
+      </section>
     </div>
   );
 };
